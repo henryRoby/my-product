e48b377dc0ba66005819f39bf2b3b50983bbe043
Henri <nambinintsoajeanyves@gmail.com>
2019-03-12 16:08:42 +0300

    version 2

--- src/App.js
+++ src/App.js
@@ -1,64 +1,77 @@
-import React, { useState } from 'react';
-
-import Forme from './Components/forme';
-import Tableau from './Components/tableau';
+import React, { useState, Fragment } from 'react'
+import AddUserForm from './Components/AddUserForm'
 import EditUserForm from './Components/EditUserForm'
-import './App.css';
+import UserTable from './Components/UserTable'
 
 const App = () => {
-    const usersData = []
-  
-    const [ users, setUsers ] = useState(usersData)
+  // Data
+  const usersData = []
 
-    const [count, setCount] = useState(0);
+  const initialFormState = { id: null, name: '', username: '' }
 
-    const ajouter = user => {
-      user.id = count
+  // Setting state
+  const [ users, setUsers ] = useState(usersData)
+  const [ currentUser, setCurrentUser ] = useState(initialFormState)
+  const [ editing, setEditing ] = useState(false)
+
+  // CRUD operations
+  const addUser = user => {
+    if (!isNaN(user.username)) {
+      user.id = users.length + 1
       setUsers([ ...users, user ])
+    }else{
+
+          document.getElementById("demo").innerHTML = "Entrer un nombre";
+  
     }
-
-    const deleteUser = id => {
-        setUsers(users.filter(user => user.id !== id))
-    }
-
-    const [ editing, setEditing ] = useState(false)
-
-    const initialFormState = { id: null,  username: '' }
-
-    const [ currentUser, setCurrentUser ] = useState(initialFormState)
-
-    const editRow = user => {
-      setEditing(true)
     
-      setCurrentUser({ id: user.id, username: user.prenom })
-    }
+  }
 
-    const updateUser = (id, updatedUser) => {
-      setEditing(false)
-    
-      setUsers(users.map(user => (user.id === id ? updatedUser : user)))
-    }
+  const deleteUser = id => {
+    setEditing(false)
 
-    return (
-      <div id="corps" className="container">
-        <div className="flex-row">
-        
-            <div className="flex-large">
-            {editing ? (
-              <div>
-                <EditUserForm editing={editing} setEditing={setEditing} currentUser={currentUser} updateUser={updateUser}
-                />
-              </div>
-            ) : (
-              <Forme ajouter={ajouter} count={count} setCount={setCount} />
-              )}
-            </div>
-            <div className="flex-large">
-              <Tableau users={users} editRow={editRow} deleteUser={deleteUser}/>
-            </div>
+    setUsers(users.filter(user => user.id !== id))
+
+
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
+  const editRow = user => {
+    setEditing(true)
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  return (
+    <div className="container">
+      <div className="flex-row">
+        <div className="flex-large">
+          {editing ? (
+            <Fragment>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <AddUserForm addUser={addUser} />
+            </Fragment>
+          )}
         </div>
+        <div className="flex-large">
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+        </div>
+      </div>
     </div>
-    ) 
-  };
+  )
+}
 
-export default App;
+export default App
